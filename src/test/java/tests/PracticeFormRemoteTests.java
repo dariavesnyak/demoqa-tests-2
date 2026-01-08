21264a5e44316b34e248f23811672f6016dd2517
daria <dariavesnyak@gmail.com>
2026-01-08 23:02:41 +0300

    added test with tag demoqa

---
+++ src/test/java/tests/PracticeFormRemoteTests.java
@@ -0,0 +1,76 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
+public class PracticeFormRemoteTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl = "https://demoqa.com";
+
+    }
+
+    @Test
+    @Tag("demoqa")
+    void fillFormTest() {
+        step("Открыть форму", () -> {
+            open("/automation-practice-form");
+        });
+        step("Заполнить все поля формы", () -> {
+            $("#firstName").setValue("Daria");
+            $("#lastName").setValue("Test");
+            $("#userEmail").setValue("daria@test.com");
+            $("#genterWrapper").$(byText("Female")).click();
+            $("#userNumber").setValue("7999111223");
+
+            //выбор даты в календаре
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__month-select").selectOption("January");
+            $(".react-datepicker__year-select").selectOption("1998");
+            $("div[aria-label='Choose Saturday, January 31st, 1998']").click();
+
+            $("#subjectsInput").setValue("Math").pressEnter();
+            $("#subjects-label").click();
+
+            $("#hobbiesWrapper").$(byText("Music")).click();
+
+            //загрузка файла
+            $("#uploadPicture").uploadFromClasspath("test.jpg");
+            $("#state").click();
+            $("#react-select-3-option-2").click();
+            $("#city").click();
+            $("#react-select-4-option-0").click();
+
+            $("#currentAddress").setValue("Lenina Street 102");
+        });
+        step("Нажать кнопку Submit", () -> {
+            $("#submit").click();
+        });
+        step("Проверка заполненной формы", () -> {
+            $(".table-responsive").shouldHave(text("Daria Test"));
+            $(".table-responsive").shouldHave(text("daria@test.com"));
+            $(".table-responsive").shouldHave(text("Female"));
+            $(".table-responsive").shouldHave(text("7999111223"));
+            $(".table-responsive").shouldHave(text("31 January,1998"));
+            $(".table-responsive").shouldHave(text("Maths"));
+            $(".table-responsive").shouldHave(text("Music"));
+            $(".table-responsive").shouldHave(text("test.jpg"));
+            $(".table-responsive").shouldHave(text("Lenina Street 102"));
+            $(".table-responsive").shouldHave(text("Haryana Karnal"));
+
+        });
+
+
+    }
+}
